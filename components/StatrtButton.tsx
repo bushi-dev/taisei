d439fdb7510e5f098ae81610cc49fd80169c7b6e
bushi-dev <ytakeshi_7@yahoo.co.jp>
2024-12-29 20:18:36 +0900

    背景を設定

---
+++ components/StatrtButton.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+import "./StatrtButton.css";
+
+const StatrtButton = () => {
+  return <button className="reset-button">はじめから</button>;
+};
+export default StatrtButton;
